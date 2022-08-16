7fb88ba25c55c00ba5bf64ec268d3b03bb4d63a6
Eric0718 <1434797171@qq.com>
2022-08-16 09:31:52 +0800

    update

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,27 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("hardhat-gas-reporter")
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    rinkeby:{
+      url: RINKEBY_RPC_URL,
+      chainid: 4,
+      accounts:[PRIVATE_KEY]
+    },
+    localhost: {
+      url: "http://localhost:8545",
+      chainId: 31337,
+    },
+  },
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+  },
 };
